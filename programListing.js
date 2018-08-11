6e5fe1d7192838a5c670f068415e967a23ff2f7f
emiminoiu <39740410+emiminoiu@users.noreply.github.com>
2018-08-11 19:09:12 +0100

    Create programListing.js

---
+++ programListing.js
@@ -0,0 +1,12 @@
+app.directive('programListing',function(){
+  return{
+         restrict:'E',
+         scope: {
+           listing: '='
+         },
+        templateUrl: 'js/directives/programListing.html'
+             
+ }            
+              
+              
+});
